c7e62241f769252296d667656f68e8fd00d8fdb3
Gustavo Andrade <gustavo.guimaraes@quicko.com.br>
2020-08-13 16:34:32 -0300

    feat: adds json and plain text return

--- backend/src/controllers/IpController.js
+++ backend/src/controllers/IpController.js
@@ -3,25 +3,37 @@ let extIP = require('ext-ip')();
 module.exports = {
   async index(req, res) {
     var forwardedIpsStr = req.header('x-forwarded-for');
-
     if (forwardedIpsStr) {
       // 'x-forwarded-for' header may return multiple IP addresses in
       // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
       // the first one
-      var forwardedIps = forwardedIpsStr.split(',');
-      ipAddress = forwardedIps[0];
+      const forwardedIps = forwardedIpsStr.split(',');
+      const ipAddress = forwardedIps[0];
 
       res.statusCode = 200;
-      return res.json({
-        ip: ipAddress
-      });
+
+      if (req.query.format === 'json') {
+        return res.json({
+          ip: ipAddress
+        });
+      } else {
+        return res
+          .set('Content-Type', 'text/html')
+          .send(`<pre>${ipAddress}</pre>`);
+      }
     } else {
       extIP.get().then(
         ip => {
           res.statusCode = 200;
-          return res.json({
-            ip: ip
-          });
+          if (req.query.format === 'json') {
+            return res.json({
+              ip: ip
+            });
+          } else {
+            return res
+              .set('Content-Type', 'text/html')
+              .send(`<pre>${ip}</pre>`);
+          }
         },
         err => {
           res.statusCode = 404;
