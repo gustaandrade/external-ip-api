85d08d3dac02b80903df91a5357e599d344e9549
Gustavo Andrade <gustavo.guimaraes@quicko.com.br>
2020-08-13 16:26:17 -0300

    fix: adds failsafe to heroku/proxy

--- backend/src/controllers/IpController.js
+++ backend/src/controllers/IpController.js
@@ -2,21 +2,35 @@ let extIP = require('ext-ip')();
 
 module.exports = {
   async index(req, res) {
-    extIP.get().then(
-      ip => {
-        res.statusCode = 200;
-        return res.json({
-          ip: ip
-        });
-      },
-      err => {
-        res.statusCode = 404;
-        return res.json({
-          error: err
-        });
-      }
-    );
+    var forwardedIpsStr = req.header('x-forwarded-for');
 
+    if (forwardedIpsStr) {
+      // 'x-forwarded-for' header may return multiple IP addresses in
+      // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
+      // the first one
+      var forwardedIps = forwardedIpsStr.split(',');
+      ipAddress = forwardedIps[0];
+
+      res.statusCode = 200;
+      return res.json({
+        ip: ipAddress
+      });
+    } else {
+      extIP.get().then(
+        ip => {
+          res.statusCode = 200;
+          return res.json({
+            ip: ip
+          });
+        },
+        err => {
+          res.statusCode = 404;
+          return res.json({
+            error: err
+          });
+        }
+      );
+    }
     return res.status(200);
   }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+
 const port = process.env.PORT || 3333;
 
 const app = express();
@@ -8,7 +9,6 @@ const app = express();
 const server = require('http').Server(app);
 
 app.use(cors());
-
 app.use(routes);
 
 server.listen(port);
